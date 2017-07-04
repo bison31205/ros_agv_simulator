753933c0e63bae455e24f344ba7f2a98579ee183
stribor14 <mirkokot@gmail.com>
2017-07-04 22:20:04 +0200

    Preparing new launch files

--- src/clock_node.py
+++ src/clock_node.py
@@ -16,6 +16,7 @@ class ClockNode:
         self.robotList = rospy.get_param('robot_list')
         self.time_modifier = float(rospy.get_param('~time_modifier', 5))
         self.clocks_per_second = float(rospy.get_param('~clocks_per_second', 30))
+        
         self.frame_duration = 1.0 / self.clocks_per_second
         
         self.clock_pub = rospy.Publisher("clock", Clock, queue_size=10)
@@ -24,6 +25,7 @@ class ClockNode:
         self.goal_sub = dict()
         self.planner_active = dict()
         self.pause_clock = False
+        
         
         for robot in self.robotList:
             self.plan_sub[robot] = rospy.Subscriber(robot + "/plan",
@@ -48,10 +50,9 @@ class ClockNode:
         if not rospy.is_shutdown():
             threading.Timer(self.frame_duration, self.main).start()
         if not self.pause_clock:
-                msg = Clock()
-                msg.clock = msg.clock.from_sec(rospy.get_time() + self.frame_duration*self.time_modifier)                        
-                self.clock_pub.publish(msg)
-           
+            msg = Clock()
+            msg.clock = msg.clock.from_sec(rospy.get_time() + self.frame_duration*self.time_modifier)                        
+            self.clock_pub.publish(msg)
 
 if __name__ == '__main__':
     clock_node = ClockNode()
